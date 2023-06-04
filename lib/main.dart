3ae73ce61705244069a1e0db9d76b6eae5092229
peerapattop <peerapatza401@gmail.com>
2023-06-05 00:53:05 +0700

    Stateless Widget,Center Widget,กำหนดขนาดและสีข้อความ,นำภาพเข้ามาทำงาน (External)

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter"),
+  runApp(MyApp());
+}
+
+//สร้าง Widget ด้วย StatelessWidget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ยินดีต้อนรับ"),
+        ),
+        body: Center(
+            // child: Text(
+            //   "พีรพัฒน์ ตั้งปัญญาไว",
+            //   style: TextStyle(fontSize: 30, color: Colors.red),
+            // ),
+            child: Image(
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2023/05/19/13/28/bird-8004544_1280.jpg"),
+            )),
       ),
-      body: Text("พีรพัฒน์ ตั้งปัญญาไว"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.green),
-  );
-  runApp(app);
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
 }
